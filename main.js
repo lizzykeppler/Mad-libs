209f9a55aed205653a3486dc6522ea286bb195ce
Elizabeth Keppler <lizzykeppler@gmail.com>
2019-01-17 21:09:48 -0800

    midlib update

---
+++ main.js
@@ -0,0 +1,6 @@
+var nounInput = document.getElementById('noun');
+var verbInput = document.getElementById('verb');
+var verbingInput = document.getElementById('verb-ing');
+
+var madLib =  nounInput + 'is when he' + verbInput + 'you a piece of your soul, that you never knew was' + verbingInput;
+console.log (madLib);
